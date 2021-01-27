c9ddbbe0e3c24f0afc6841b9bd2ed17b85fca8f1
Nick Sawyer <sawyer3nick@gmail.com>
2021-01-26 21:34:42 -0800

    Data separated into training, verification, and testing.  Used sklearn to parse into a tf-idf bag of words with unigrams, bigrams, and trigrams.

--- create_bag_of_words.py
+++ create_bag_of_words.py
@@ -1,8 +1,10 @@
 from sys import argv
 from pickle import load,dump
 from collections import defaultdict
+from sklearn.feature_extraction.text import TfidfVectorizer
+from random import shuffle
 '''
-Creates a trigram bag of words based on the descriptions of the movie_dict
+Creates a trigram bag of words based on the descriptions of the movie_dict. Note, when run it will override pickled_bag_of_words with new randomly selected values
 '''
 if (len(argv) != 2 ):
     raise Exception("Expected 1 argument received {}".format(len(argv)-1)) 
@@ -10,18 +12,50 @@ file = open(argv[1],"rb")
 
 movie_dict = load(file)
 
-def trigram(index:int,array: list,):
-    if index+2 < len(array):
-        return "{}{}{}".format(array[index].strip(".,;:'\"?!"),array[index+1].strip(".,;:'\"?!"),array[index+2].strip(".,;:'\"?!"))
-    return ""
-
+# def trigram(index:int,array: list,):
+#     if index+2 < len(array):
+#         return "{}{}{}".format(array[index].strip(".,;:'\"?!"),array[index+1].strip(".,;:'\"?!"),array[index+2].strip(".,;:'\"?!"))
+#     return ""
+print("Creating Bag of Words")
 bag_of_words = defaultdict(int)
-for key in movie_dict.keys():
-    temp_sentence =  movie_dict[key]["description"].split(" ")
-    for i in range(len(temp_sentence)):
-        if trigram(i,temp_sentence) != "":
-            bag_of_words[trigram(i,temp_sentence)]+=1 
+random_list = list(range(0,len(movie_dict.keys())))
+shuffle(random_list)
+training_size = int(.6*len(random_list))
+verifying_size = int(.8*len(random_list))
+k = list(movie_dict.keys())
 
-outfile = open("./data/pickled_bag_of_words","wb")
+training_data = []
+verifying_data = []
+testing_data = []
+print("Selecting Training Data")
+for index in range(0,training_size):
+    training_data.append(movie_dict[k[random_list[index]]]["description"])
+print("Selecting Verification Data")
+for index in range(training_size,verifying_size):
+    verifying_data.append(movie_dict[k[random_list[index]]]["description"])
+print("Selecting Testing Data")
+for index in range(verifying_size,len(random_list)):
+    testing_data.append(movie_dict[k[random_list[index]]]["description"])
 
-dump(bag_of_words,outfile)
+print("Vectorizing Training Data")
+training_vector = TfidfVectorizer(ngram_range=(1,3))
+print("Vectorizing Verification Data")
+verifying_vector = TfidfVectorizer(ngram_range=(1,3))
+print("Vectorizing Testing Data")
+testing_vector = TfidfVectorizer(ngram_range=(1,3))
+
+print("Determining idf and vocabulary for Training Data")
+training_vector.fit_transform(training_data)
+print("Determining idf and vocabulary for Verification Data")
+verifying_vector.fit_transform(verifying_data)
+print("Determining idf and vocabulary for Testing Data")
+t= testing_vector.fit_transform(testing_data)
+
+print ("Writing Results to Files")
+training_file = open("./data/training_data_vector","wb")
+verifying_file = open("./data/verification_data_vector","wb")
+testing_file = open("./data/testing_data_vector","wb")
+dump(training_vector,training_file)
+dump(verifying_vector,verifying_file)
+dump(testing_vector,testing_file)
+print("Finished Creating Bag of Words")
